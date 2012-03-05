ac6ef6775201a6bf8377dcb47541b7539a9141c8
Flip <ph.seiffert@gmx.de>
2012-03-05 17:24:09 +0100

    updated the documentation

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
 	<!-- CSS
   ================================================== -->
-  	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
+
 	<link rel="stylesheet" href= "<?php echo get_template_directory_uri(); ?>/style.css">
 	
 
